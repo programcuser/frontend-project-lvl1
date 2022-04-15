2392cbc11633f7b8e1e7561200a3118ee2794659
Mr Smith <krygrish@yandex.ru>
2022-04-15 17:45:45 +0300

    Add brain-prime game

---
+++ src/games/prime-game.js
@@ -0,0 +1,50 @@
+import greeting from '../cli.js';
+import * as game from '../index.js';
+
+const isPrime = (number) => {
+  if (number === 1) {
+    return false;
+  }
+
+  let i = Math.ceil(number / 2);
+  while (i > 1) {
+    if (number % i === 0) {
+      return false;
+    }
+    i -= 1;
+  }
+
+  return true;
+};
+
+const startPrimeGame = (rounds) => {
+  const name = greeting();
+  game.printRule('Answer "yes" if given number is prime. Otherwise answer "no".');
+
+  let isRightAnswer = true;
+  let i = 1;
+  while (i <= rounds && isRightAnswer) {
+    const num = game.getRandomInt(1, 100);
+
+    console.log(`Question: ${num}`);
+    const userAnswer = game.getAnswer();
+    const resultAnswer = isPrime(num) ? 'yes' : 'no';
+
+    isRightAnswer = (userAnswer === resultAnswer);
+    if (!isRightAnswer) {
+      console.log(`'${userAnswer}' is wrong answer ;(. Correct answer was '${resultAnswer}'.`);
+      break;
+    }
+    console.log('Correct!');
+
+    i += 1;
+  }
+
+  if (i === rounds + 1) {
+    console.log(`Congratulations, ${name}!`);
+  } else {
+    console.log(`Let's try again, ${name}`);
+  }
+};
+
+export default startPrimeGame;
